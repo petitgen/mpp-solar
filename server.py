6935048225ba16f01f4daa5324f03f7f1f9aa21d
Sebastien MATHIEU <sebastien.mathieu@kathmandu.co.nz>
2020-01-03 15:06:35 +1300

    New version

--- server.py
+++ server.py
@@ -3,13 +3,15 @@ from flask_restful import Resource, Api
 from json import dumps
 from flask_jsonpify import jsonify
 
-from mpputils import mppUtils
+from mppsolar.mpputils import mppUtils
 
 app = Flask(__name__)
 api = Api(app)
 
-device="/dev/ttyUSB0"
+device='/dev/ttyUSB0'
 baud=2400
+host='0.0.0.0'
+port='5002'
 
 mp = mppUtils(device, baud)
 
@@ -17,9 +19,12 @@ class Status(Resource):
   def get(self):
     data = mp.getResponseDict("Q1")
     data.update(mp.getResponseDict("QPIGS"))
+    status = {}
+    for item in data.keys():
+        status[key] = data[key][0]
     return data
 
 api.add_resource(Status, '/status') # Route_1
 
 if __name__ == '__main__':
-     app.run(port='5002')
+     app.run(host= host, port=port)
