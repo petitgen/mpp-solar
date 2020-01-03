fbb4265d53a60bd46cfcf6c3e020402b59c9a894
Sebastien MATHIEU <sebastien.mathieu@kathmandu.co.nz>
2020-01-03 14:53:12 +1300

    New version

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from json import dumps
-from flask.jsonpify import jsonify
+from flask_jsonpify import jsonify
 
 from mpputils import mppUtils
 
